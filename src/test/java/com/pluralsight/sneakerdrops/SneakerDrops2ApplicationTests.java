e97bf861fb2c1f5df40e39163ed7ae01653fd151
keziazahiljohn <keziazahiljohn@gmail.com>
2026-06-10 08:45:50 -0400

    exercise completed

--- src/test/java/com/pluralsight/sneakerdrops/SneakerDrops2ApplicationTests.java
+++ src/test/java/com/pluralsight/sneakerdrops/SneakerDrops2ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SneakerDrops2ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
